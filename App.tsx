bf7d8a9ec0f5ad24cc6d6de90beed8434e13d96f
AndrRum <andrey_rumiantsev95@mail.ru>
2021-01-13 19:46:34 +0200

    add splash in app

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import {Container} from './src/navigation/NavigationContainer'
-import {store} from './src/store/store'
-import {Provider} from 'react-redux'
+import React, {useEffect} from "react";
+import {Container} from "./src/navigation/NavigationContainer";
+import {store} from "./src/store/store";
+import {Provider} from "react-redux";
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    // Hide SplashScreen after 3secs or Make an async task
+    setTimeout(() => {
+      RNBootSplash.hide({ fade: true });
+    }, 2000);
+  }, []);
   return(
     <Provider store = {store}>
         <Container/>
     </Provider>
-  )
-}
+  );
+};
 
 
 export default App;
-
