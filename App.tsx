773fe4f17839671cdd8eefad6fe4b4338c399af1
AndrRum <andrey.rumiantsev95@gmail.com>
2021-01-22 17:12:40 +0200

    remove boodsplash

--- App.tsx
+++ App.tsx
@@ -1,21 +1,16 @@
-import React, {useEffect} from "react";
+import React from "react";
 import {Container} from "./src/navigation/NavigationContainer";
 import {store} from "./src/store/store";
 import {Provider} from "react-redux";
-import RNBootSplash from 'react-native-bootsplash';
+
 
 const App = () => {
-  useEffect(() => {
-    // Hide SplashScreen after 3secs or Make an async task
-    setTimeout(() => {
-      RNBootSplash.hide({ fade: true });
-    }, 2000);
-  }, []);
-  return(
+
+  return (
     <Provider store = {store}>
-        <Container/>
+      <Container/>
     </Provider>
-  );
+  )
 };
 
 
