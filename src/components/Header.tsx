972d9597aa5e8049cee0bafe73c440d461f9c5b4
AndrRum <andrey.rumiantsev95@gmail.com>
2021-02-12 17:03:28 +0200

    Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import { View, Text, Image, } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
+import styles from "./styles";
+
+export const Header = () => {
+    const navigation = useNavigation();
+    return (
+        <View style={styles.container}>
+            <View >
+                <TouchableOpacity onPress={()=> {navigation.goBack()}}
+                                  style={styles.backButton}
+                >
+                    <Image
+                        style={styles.imageButton}
+                        source={require("../assets/pictures/icon_back_click.png")}
+                    />
+                </TouchableOpacity>
+            </View>
+            <View style={styles.viewHeaderText}>
+                <Text style={styles.headerText}>CoffeTime</Text>
+            </View>
+        </View>
+    );
+  };
